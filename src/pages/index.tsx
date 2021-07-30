1f00311765e97cef50a40b55b91bcd3cffbdbabd
Fábio <fabio.mendes@profusion.com>
2021-07-29 21:27:06 -0300

    add: global styles, fonts and title for index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <h1>Home</h1>
-  )
+    <>
+      <Head>
+        <title></title>
+      </Head>
+      <h1>Home</h1>
+    </>
+  );
 }
