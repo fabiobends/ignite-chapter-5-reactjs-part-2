f505fa77fb103034c1a0c92f24f328d0a8153615
Fábio <fabio.mendes@profusion.com>
2021-07-30 22:06:25 -0300

    add: header and favicon

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from "./styles.module.scss";
+import Image from "next/image";
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
