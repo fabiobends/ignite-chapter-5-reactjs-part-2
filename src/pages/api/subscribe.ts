d3e2ade39c6d48c30b9474414dbe6c485a4f439b
Fábio <fabio.mendes@profusion.com>
2021-08-08 23:20:50 -0300

    add: stripe checkout process verifying duplicate subscriptions

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,59 @@
+import { NextApiRequest } from "next";
+import { getSession } from "next-auth/client";
+import { NextApiResponse } from "next-auth/internals/utils";
+import { fauna } from "../../services/fauna";
+import { query as q } from "faunadb";
+import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const session = await getSession({ req });
+
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      });
+
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      customerId = stripeCustomer.id;
+    }
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: customerId,
+      allow_promotion_codes: true,
+      billing_address_collection: "required",
+      payment_method_types: ["card"],
+      mode: "subscription",
+      line_items: [{ price: "price_1JJS4dDX5lsnpe4x9ev7OgGt", quantity: 1 }],
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+    });
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method is allowed");
+  }
+};
