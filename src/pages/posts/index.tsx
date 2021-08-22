d5f6c822d77fd72d454f1bef18c516f479a1bec5
Fábio <fabio.mendes@profusion.com>
2021-08-21 22:16:35 -0300

    add: posts with requested data

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,21 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from "@prismicio/client";
 import styles from "./styles.module.scss";
+import { RichText } from "prismic-dom";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -9,35 +23,46 @@ export default function Posts() {
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time></time>
-            <strong>Mapas com React usando Leaflet</strong>
-            <p>
-              Neste post vamos desenvolver uma página web para demonstrar, na
-              prática, a integração de Mapas em uma aplicação com React
-              usando Leaflet.
-            </p>
-          </a>
-          <a href="#">
-            <time></time>
-            <strong>Mapas com React usando Leaflet</strong>
-            <p>
-              Neste post vamos desenvolver uma página web para demonstrar, na
-              prática, a integração de Mapas em uma aplicação com React
-              usando Leaflet.
-            </p>
-          </a>
-          <a href="#">
-            <time></time>
-            <strong>Mapas com React usando Leaflet</strong>
-            <p>
-              Neste post vamos desenvolver uma página web para demonstrar, na
-              prática, a integração de Mapas em uma aplicação com React
-              usando Leaflet.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a href="#" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const response = await prismic.query(
+    Prismic.Predicates.at("document.type", "post"),
+    { fetch: ["post.title", " post.content"], pageSize: 100 }
+  );
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === "paragraph")
+          ?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "pt-BR",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+  return {
+    props: {
+      posts,
+    },
+  };
+};
