819039c3bb5a6311caed7d68c2679e43554f239f
Fábio <fabio.mendes@profusion.com>
2021-08-23 21:49:45 -0300

    add: validating subscription

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import { query as q } from "faunadb";
 import { fauna } from "../../../services/fauna";
+import { session } from "next-auth/client";
 
 export default NextAuth({
   providers: [
@@ -12,6 +13,32 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index("user_by_email"),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+        return { ...session, activeSubscription: userActiveSubscription };
+      } catch {
+        return { ...session, activeSubscription: null };
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user;
 
@@ -19,12 +46,7 @@ export default NextAuth({
         await fauna.query(
           q.If(
             q.Not(
-              q.Exists(
-                q.Match(
-                  q.Index("user_by_email"),
-                  q.Casefold(email)
-                )
-              )
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
             ),
             q.Create(q.Collection("users"), {
               data: { email },
